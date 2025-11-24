9ffa4f3ff1e244f4bf265cd473eb0a08e732d5e5
Hector Lugo <hlugogabino@gmail.com>
2025-11-24 15:55:27 -0600

    Simulacion con traffic model

--- AgentsVisualization/assets/shaders/vs_color.glsl
+++ AgentsVisualization/assets/shaders/vs_color.glsl
@@ -3,10 +3,12 @@ in vec4 a_position;
 in vec4 a_color;
 
 uniform mat4 u_transforms;
+uniform vec4 u_color;
 
 out vec4 v_color;
 
 void main() {
     gl_Position = u_transforms * a_position;
-    v_color = a_color;
+    // Use uniform color if provided, otherwise use vertex color
+    v_color = u_color.a > 0.0 ? u_color : a_color;
 }
